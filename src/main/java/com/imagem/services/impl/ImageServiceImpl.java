6843dc07db0e66a51436d385fd3484c49770cf23
Jefferson <jefferson.luiz.cruz@gmail.com>
2021-05-28 01:32:25 -0300

    created the service implemantation and validation

--- src/main/java/com/imagem/services/impl/ImageServiceImpl.java
+++ src/main/java/com/imagem/services/impl/ImageServiceImpl.java
@@ -1,11 +1,14 @@
 package com.imagem.services.impl;
 
 
+import java.io.IOException;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
 import com.imagem.entities.Image;
+import com.imagem.entities.Image.ImageBuilder;
 import com.imagem.repositories.ImageRepository;
 import com.imagem.services.ImageService;
 import com.imagem.services.exceptions.BadRequestException;
@@ -17,15 +20,26 @@ public class ImageServiceImpl implements ImageService{
 
 	@Override
 	public Image save(MultipartFile image) {
-		uploadImage(image);
+		try {
+			return uploadImage(image);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
 		return null;
 	}
 
-	private void uploadImage(MultipartFile image) {
+	private Image uploadImage(MultipartFile image) throws IOException {
+		byte[] data = image.getBytes();
 		String type = image.getContentType();
 		String fileName = image.getOriginalFilename();
 		
 		validateImageName(fileName);
+		
+		Image images = imageRepository.saveAndFlush(ImageBuilder.newBuilder(data)
+												  				.withType(type)
+												  				.withFileName(fileName)
+												  				.build());
+		return images;
 	}
 	
 	private void validateImageName(String fileName) {
